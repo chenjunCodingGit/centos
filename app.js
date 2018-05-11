14fa33ea5065672d8ba3f1fa65d00c75ebc21505
chenjunCodingGit <271676725@qq.com>
2018-05-12 01:04:49 +0800

    get post

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
+app.get('/get', function (req, res) {
+  res.send('yes');
+});
 
-app.listen(8080, () => {
-  console.log(`App listening at port 8080`)
+app.post('/data', function (req, res) {
+  res.send('5555 post');
+});
+
+app.listen(3030, () => {
+  console.log(`App listening at port 3030`)
 })
